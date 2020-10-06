a64a4c46b4d88a431f524c1df0c6767a9210bb9c
Arunachalam3197 <arunachalam3197@gmail.com>
2020-10-06 16:25:15 +0530

    Class

---
+++ fullstackdeveloper/src/main/java/fullstack/java/fullstackdeveloper/App.java
@@ -0,0 +1,13 @@
+package fullstack.java.fullstackdeveloper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("Basic Program");
+    }
+}
